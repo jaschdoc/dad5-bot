19d01699f3081e7fd6df42928885af30f24a0ae2
fjerbaek <jacob.f.olsen@gmail.com>
2020-11-20 17:50:54 +0100

    updated values and added actual db support

--- models/klandring.js
+++ models/klandring.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const klandringSchema = mongoose.Schema({
     klandret: String,
     klandrer : String,
+    key: String,
     isDecided: Boolean,
     verdict: Number
      
---
+++ utils/klandringer.js
@@ -0,0 +1,35 @@
+const dbHandler = require('../utils/dbhandler.js');
+const Klandring = require('../models/klandring.js');
+
+module.exports = {
+    addKlandring : addKlandring,
+    print : print
+}
+
+
+async function addKlandring(klandrer, klandret, key){
+    dbhandler.insertOne()
+}
+
+
+async function print(){
+    const klandringer = await getKlandringer(); 
+    let msg = "";
+    klandringer.forEach(klandring => {
+        msg += "Klandrer: " + klandring.klandret + " Klandret:" + klandring.klandrer + " Key:" + klandring.key;
+    });
+    return msg + "Here."
+}
+
+async function getKlandringer(){
+    const klandringer = await dbHandler.find(Klandring, {}).catch(err => dbError());
+    return klandringer;
+}
+
+async function add(klandrer, klandret, key){
+    await dbHandler.insertOne(Klandring, {"klandrer":klandrer, "klandret":klandret, "key":key}).catch(err => dbError());
+}
+
+function dbError(){
+    console.log("Error communicating with db");
+}
