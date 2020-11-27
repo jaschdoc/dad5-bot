cfc3f24bc2f7623566e9d845ffb334caf1f42a01
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2020-11-27 17:09:56 +0100

    Add Help Test

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,14 @@
+const { prefix } = require('../config.json');
 module.exports = {
-	name: 'help',
-	usage: '[command]',
-	description: 'Prints help information for commands',
-    alias:['h', 'yelp', 'techsupport', 'commands'],
-	async execute(message, args) {
-        
-	}
+    name: 'help',
+    usage: '[command]',
+    description: 'Prints help information for commands',
+    alias: ['h', 'yelp', 'techsupport', 'commands'],
+    async execute(message, args) {
+        const reply = new Array();
+        const { commands } = message.client;
+
+        return message.reply('test');
+    }
 };
 
