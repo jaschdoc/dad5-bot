12c927a178e649368143999455520725791cf01e
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2020-12-19 20:03:02 +0100

    Update Description

--- src/commands/blame.ts
+++ src/commands/blame.ts
@@ -4,7 +4,7 @@ import { Command, Message } from './commands.interface';
 export const blame: Command = {
     name: 'blame',
     usage: '',
-    description: 'Handles \'blame\'',
+    description: 'Handles blame',
     alias: ['klandring'],
     args: false,
     async execute(message: Message, args: string[]) {
